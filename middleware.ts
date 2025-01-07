dbba9c28ae84b0c37c94e228df6d6a50626e98b5
Raj Thombare <therajthombare@gmail.com>
2025-01-07 22:01:35 +0530

    fix: auth button

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,13 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-// const isProtectedRoute = createRouteMatcher(['/dashboard(.*)'])
-const isProtectedRoute = createRouteMatcher(['/store(.*)'])
+const isProtectedRoute = createRouteMatcher(['/dashboard(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
-    if (isProtectedRoute(req)) await auth.protect()
+    const { userId, redirectToSignIn } = await auth()
+
+    if (!userId && isProtectedRoute(req)) {
+        return redirectToSignIn()
+    }
 })
 
 export const config = {
